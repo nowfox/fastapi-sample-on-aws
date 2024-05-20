b8e1ff9742b86ecacbcd1997766a16f0798c867d
Junzhong <junzhong@amazon.com>
2024-05-20 08:37:41 +0000

    feat: add log

--- source/api/main.py
+++ source/api/main.py
@@ -1,9 +1,14 @@
 import os
+import logging.config
 from mangum import Mangum
 from fastapi import FastAPI, HTTPException, Depends, Security
 from fastapi.security import APIKeyHeader
 from starlette import status
 from pet.main import router as pet_router
+from common import constant
+
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(constant.LOGGER_API)
 
 X_API_KEY_HEADER = APIKeyHeader(name='X-API-Key')
 X_API_KEY = os.getenv('X-API-Key','1234')
@@ -18,7 +23,7 @@ def check_authentication_header(x_api_key: str = Depends(X_API_KEY_HEADER)):
 
 stage = os.getenv('Stage')
 root_path = f"/{stage}" if stage else ""
-
+logger.info(f"root_path={root_path}")
 app = FastAPI(title="FastAPI Sample on AWS",
               version="v0.1.0",
               dependencies=[Security(check_authentication_header)],
